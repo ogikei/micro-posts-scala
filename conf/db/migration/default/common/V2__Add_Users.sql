465d4f73b4097cbc2451bb61f5a6a12d9b293714
Keisuke Ogiwara <keisukeogiwara@KeisukenoMacBook-puro.local>
2018-08-25 21:24:02 +0900

    add Controller and View of User

---
+++ conf/db/migration/default/common/V2__Add_Users.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test1', 'test1@test.com', '$2a$10$.W5ToqLvw0qSQPQZcpF9uuq9.uhq5pDZTG1Rnh8XXCjCzRhKZ7tn2', now(), now());
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test2', 'test2@test.com', '$2a$10$.W5ToqLvw0qSQPQZcpF9uulHyTlz8YdPinQlpZOP77F89rM9IWxx.', now(), now());
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test3', 'test3@test.com', '$2a$10$.W5ToqLvw0qSQPQZcpF9uuhqCYY2119iErPEGC/xd3ynhr8o8UnNS', now(), now());
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test4', 'test4@test.com', '$2a$10$.W5ToqLvw0qSQPQZcpF9uuJPDrSpeu/1rLzvTWBudb.dyax7swHu.', now(), now());
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test5', 'test5@test.com', '$2a$10$.W5ToqLvw0qSQPQZcpF9uuRQv2UlgQlAsxBwqbzdNgTog4sgbFqYu', now(), now());
+INSERT INTO users (name, email, password, create_at, update_at) VALUES ('test6', 'test6@test.com', '$2a$10$.W5ToqLvw0qSQPQZcpF9uu8.59RpnEcgAQI4angPMRm8gJZz5wmV2', now(), now());
