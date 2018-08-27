aebd49ee6eb022b7efeff61c11ba323ac2a6955f
Keisuke Ogiwara <keisukeogiwara@KeisukenoMacBook-puro.local>
2018-08-28 00:32:33 +0900

    timeline

--- app/controllers/UsersController.scala
+++ app/controllers/UsersController.scala
@@ -8,10 +8,14 @@ import play.api.i18n.{I18nSupport, Messages}
 import play.api.mvc._
 import skinny.Pagination
 
-import services.UserService
+import models.User
+import services.{MicroPostService, UserFollowService, UserService}
 
 @Singleton
-class UsersController @Inject()(val userService: UserService, components: ControllerComponents)
+class UsersController @Inject()(val userService: UserService,
+    val microPostService: MicroPostService,
+    val userFollowService: UserFollowService,
+    components: ControllerComponents)
     extends AbstractController(components)
         with I18nSupport
         with AuthConfigSupport
@@ -31,20 +35,93 @@ class UsersController @Inject()(val userService: UserService, components: Contro
         .getOrElse(InternalServerError(Messages("InternaError")))
   }
 
-  def show(userId: Long) = StackAction { implicit request =>
-    userService
-        .findById(userId)
-        .map { userOpt =>
-          userOpt.map { user =>
-            Ok(views.html.users.show(loggedIn, user))
-          }.get
-        }
-        .recover {
-          case e: Exception =>
-            Logger.error(s"occurred error", e)
-            Redirect(routes.UsersController.index())
-                .flashing("failure" -> Messages("InternalError"))
-        }
+  def show(userId: Long, page: Int) = StackAction { implicit request =>
+    val triedUserOpt = userService.findById(userId)
+    val triedUserFollows = userFollowService.findById(loggedIn.id.get)
+    val pagination = Pagination(10, page)
+    val triedMicroPosts = microPostService.findByUserId(pagination, userId)
+    val triedFollowingsSize = userFollowService.countByUserId(userId)
+    val triedFollowersSize = userFollowService.countByFollowId(userId)
+    (for {
+      userOpt <- triedUserOpt
+      userFollows <- triedUserFollows
+      microPosts <- triedMicroPosts
+      followingsSize <- triedFollowingsSize
+      followersSize <- triedFollowersSize
+    } yield {
+      userOpt.map { user =>
+        Ok(views.html.users.show(loggedIn, user, userFollows, microPosts, followingsSize, followersSize))
+      }.get
+    }).recover {
+      case e: Exception =>
+        Logger.error(s"occurred error", e)
+        Redirect(routes.UsersController.index())
+            .flashing("failure" -> Messages("InternalError"))
+    }
+        .getOrElse(InternalServerError(Messages("InternalError")))
+  }
+
+  def getFollowers(userId: Long, page: Int) = StackAction { implicit request =>
+    val targetUser = User.findById(userId).get
+    val triedMaybeUserFollow = userFollowService.findById(loggedIn.id.get)
+    val pagination = Pagination(10, page)
+    val triedFollowers = userFollowService.findFollowersByUserId(pagination, userId)
+    val triedMicroPostsSize = microPostService.countBy(userId)
+    val triedFollowingsSize = userFollowService.countByUserId(userId)
+    (for {
+      userFollows <- triedMaybeUserFollow
+      followers <- triedFollowers
+      microPostSize <- triedMicroPostsSize
+      followingsSize <- triedFollowingsSize
+    } yield {
+      Ok(
+        views.html.users.followers(
+          loggedIn,
+          targetUser,
+          userFollows,
+          followers,
+          microPostSize,
+          followingsSize
+        )
+      )
+    }).recover {
+      case e: Exception =>
+        Logger.error("occurred error", e)
+        Redirect(routes.UsersController.index())
+            .flashing("failure" -> Messages("InternalError"))
+    }
+        .getOrElse(InternalServerError(Messages("InternalError")))
+  }
+
+  def getFollowings(userId: Long, page: Int) = StackAction { implicit request =>
+    val targetUser = User.findById(userId).get
+    val triedUserFollows = userFollowService.findById(loggedIn.id.get)
+    val pagination = Pagination(10, page)
+    val triedFollowings = userFollowService.findFollowingsByUserId(pagination, userId)
+    val triedMicroPostsSize = microPostService.countBy(userId)
+    val triedFollowersSize = userFollowService.countByFollowId(userId)
+    (for {
+      userFollows <- triedUserFollows
+      followings <- triedFollowings
+      microPostsSize <- triedMicroPostsSize
+      followersSize <- triedFollowersSize
+    } yield {
+      Ok(
+        views.html.users.followings(
+          loggedIn,
+          targetUser,
+          userFollows,
+          followings,
+          microPostsSize,
+          followersSize
+        )
+      )
+    }).recover {
+      case e: Exception =>
+        Logger.error("occurred error", e)
+        Redirect(routes.UsersController.index())
+            .flashing("failure" -> Messages("InternalError"))
+    }
         .getOrElse(InternalServerError(Messages("InternalError")))
   }
 
