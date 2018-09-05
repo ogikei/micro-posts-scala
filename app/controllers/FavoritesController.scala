87288b3733d58d09837d01d836c184bae46e0164
Keisuke Ogiwara <keisukeogiwara@KeisukenoMacBook-puro.local>
2018-09-05 17:34:27 +0900

    add

--- app/controllers/FavoritesController.scala
+++ app/controllers/FavoritesController.scala
@@ -1,11 +1,13 @@
 package controllers
 
 import javax.inject._
+
 import jp.t2v.lab.play2.auth.AuthenticationElement
 import play.api.Logger
 import play.api.i18n.{I18nSupport, Messages}
 import play.api.mvc._
 import skinny.Pagination
+
 import models.User
 import services.{FavoriteService, MicroPostService, UserFollowService, UserService}
 
@@ -27,12 +29,12 @@ class FavoritesController @Inject()(val userService: UserService,
     val triedFollowingsSize = favoriteService.countByUserId(userId)
     (for {
       userOpt <- triedUserOpt
-      userFollows <- triedFavorites
+      favorites <- triedFavorites
       microPosts <- triedMicroPosts
       followingsSize <- triedFollowingsSize
     } yield {
       userOpt.map { user =>
-        Ok(views.html.favorite.show(loggedIn, user, userFollows, microPosts, followingsSize))
+        Ok(views.html.favorite.show(loggedIn, user, favorites, microPosts, followingsSize))
       }.get
     }).recover {
       case e: Exception =>
--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,18 +3,21 @@ package controllers
 import javax.inject._
 
 import jp.t2v.lab.play2.auth.OptionalAuthElement
-import models.{MicroPost, PagedItems}
 import play.api.Logger
 import play.api.data.Forms._
 import play.api.data._
 import play.api.i18n.{I18nSupport, Messages}
 import play.api.mvc._
-import services.{MicroPostService, UserService}
 import skinny.Pagination
 
+import models.{MicroPost, PagedItems}
+import services.{FavoriteService, MicroPostService, UserService}
+
 @Singleton
-class HomeController @Inject()(val userService: UserService,
+class HomeController @Inject()(
+    val userService: UserService,
     val microPostService: MicroPostService,
+    val favoriteService: FavoriteService,
     components: ControllerComponents)
     extends AbstractController(components)
         with I18nSupport
@@ -27,12 +30,13 @@ class HomeController @Inject()(val userService: UserService,
 
   def index(page: Int): Action[AnyContent] = StackAction { implicit request =>
     val userOpt = loggedIn
+    val favariteIds = favoriteService.countByUserId(userOpt.get.id)
     userOpt
         .map { user =>
           microPostService
               .findAllByWithLimitOffset(Pagination(10, page), user.id.get)
               .map { pagedItems =>
-                Ok(views.html.index(userOpt, postForm, pagedItems))
+                Ok(views.html.index(userOpt, postForm, pagedItems, favoriteIds))
               }
               .recover {
                 case e: Exception =>
@@ -44,7 +48,7 @@ class HomeController @Inject()(val userService: UserService,
         }
         .getOrElse(
           Ok(views.html.index(
-            userOpt, postForm, PagedItems(Pagination(10, page), 0, Seq.empty[MicroPost])))
+            userOpt, postForm, PagedItems(Pagination(10, page), 0, Seq.empty[MicroPost]), Seq.empty[Long]))
         )
   }
 
